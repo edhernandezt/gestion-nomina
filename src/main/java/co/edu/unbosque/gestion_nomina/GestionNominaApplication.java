686959f77ecd80e15c0bb559db13fa1c68ff1f8d
ehernandezt <ehernandezt@unbosque.edu.co>
2025-04-03 15:15:47 -0500

    Repositorio - Proyecto gestión de nómina

---
+++ src/main/java/co/edu/unbosque/gestion_nomina/GestionNominaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.gestion_nomina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNominaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNominaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unbosque/gestion_nomina/GestionNominaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.gestion_nomina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionNominaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
