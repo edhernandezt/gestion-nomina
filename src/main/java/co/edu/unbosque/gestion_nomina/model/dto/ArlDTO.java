1c0ec91f518fba28d5e915d28a22af6886d3aa3f
judricalderon <judricalderon@gmail.com>
2025-04-06 12:00:25 -0500

    creacion de ArlDTO

--- src/main/java/co/edu/unbosque/gestion_nomina/model/dto/ArlDTO.java
+++ src/main/java/co/edu/unbosque/gestion_nomina/model/dto/ArlDTO.java
@@ -1,4 +1,34 @@
 package co.edu.unbosque.gestion_nomina.model.dto;
 
+import jakarta.persistence.*;
+
 public class ArlDTO {
+
+
+        private Integer idArl;
+
+
+        private String nombreArl;
+
+        public ArlDTO() {}
+
+        public ArlDTO(String nombreArl) {
+            this.nombreArl = nombreArl;
+        }
+
+        public Integer getIdArl() {
+            return idArl;
+        }
+
+        public void setIdArl(Integer idArl) {
+            this.idArl = idArl;
+        }
+
+        public String getNombreArl() {
+            return nombreArl;
+        }
+
+        public void setNombreArl(String nombreArl) {
+            this.nombreArl = nombreArl;
+        }
 }
