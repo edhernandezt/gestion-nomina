8eabf8664e921e7e2f69adc9b3edc2ab2682133a
ehernandezt <ehernandezt@unbosque.edu.co>
2025-04-07 07:10:05 -0500

    CRUD Cargos

---
+++ src/main/java/co/edu/unbosque/gestion_nomina/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package co.edu.unbosque.gestion_nomina.exceptions;
+
+import co.edu.unbosque.gestion_nomina.model.dto.CargoDTO;
+import co.edu.unbosque.gestion_nomina.service.interfaces.ICrud;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @Autowired
+    private ICrud<CargoDTO, Integer> cargoService;
+
+    @ExceptionHandler(CargoException.class)
+    public String manejarCargoException(CargoException ex, Model model) {
+        model.addAttribute("mensajeError", ex.getMessage());
+        model.addAttribute("cargoDTO", new CargoDTO());
+        model.addAttribute("cargoBuscado", null);
+        model.addAttribute("cargos", cargoService.findAll());
+
+        return "cargos";
+    }
+}
